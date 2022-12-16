526958d3fce30d84b0c8b3ae2453be583d0e3c73
stefan muto <mutobui@gmail.com>
2022-12-16 10:58:52 +0700

    get token portfolio

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -2,6 +2,7 @@ import { parse } from "csv-parse";
 import { Transaction, Wallet } from "../types";
 import * as fs from "fs";
 import axios from "axios";
+import Decimal from "decimal.js";
 
 export async function build_wallet(_path: string) {
     return new Promise<Wallet>((resolve) => {
@@ -22,17 +23,35 @@ export async function build_wallet(_path: string) {
 export async function get_price(token: string) {
     let price_api = `https://min-api.cryptocompare.com/data/price?fsym=${token}&tsyms=USD`;
     const response = await axios.get(price_api);
-    return response.data.USD;
+    return new Decimal(response.data.USD);
 }
 
 /// Given no parameters, return the latest portfolio value per token in USD
-export function get_latest_portfolio() {
+export async function get_latest_portfolio(resource_path: string) {
+    let wallet = await build_wallet(resource_path);
+    let portfolio = [];
 
+    for (let i = 0; i < wallet.data.length; i++) {
+        let token_data = wallet.data[i];
+        let price = await get_price(token_data.token);
+        portfolio.push({
+            token: token_data.token,
+            balance: token_data.balance.mul(price),
+        })
+    }
+
+    return portfolio;
 }
 
 /// Given a token, return the latest portfolio value for that token in USD
-export function get_latest_portfolio_by_token() {
-
+export async function get_latest_portfolio_by_token(token: string, source_path: string) {
+    let portfolio = await get_latest_portfolio(source_path);
+    for (let i = 0; i < portfolio.length; i++) {
+        if (portfolio[i].token === token) {
+            return portfolio[i];
+        }
+    }
+    return null;
 }
 
 /// Given a date, return the portfolio value per token in USD on that date
--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -1,8 +1,10 @@
 import test from 'ava';
 import Decimal from 'decimal.js';
-import { build_wallet, get_price } from '../parser';
+import {
+    build_wallet, get_latest_portfolio,
+    get_latest_portfolio_by_token, get_price
+} from '../parser';
 
-// import { read_transaction } from '../parser/parser';
 import { Wallet } from '../types/token';
 import { Transaction } from '../types/transaction';
 
@@ -17,6 +19,7 @@ const test_data = [
     ["1571966896", "WITHDRAWAL", "XRP", "0.819840"],
     ["1571966868", "WITHDRAWAL", "XRP", "0.969999"],
     ["1571966849", "WITHDRAWAL", "XRP", "0.650535"],
+    ["1571966840", "DEPOSIT", "XRP", "3.650535"],
 ]
 
 test('Build Wallet', async (t) => {
@@ -44,7 +47,7 @@ test('Get Balance', async (t) => {
         },
         {
             token: "XRP",
-            balance: new Decimal(-2.140888),
+            balance: new Decimal(1.509647),
         }
     ]
     let path = "src/tests/transaction_test.csv";
@@ -58,20 +61,75 @@ test('Get Price', async (t) => {
         let token = "BTC";
         let price = await get_price(token);
         let bear_price = 10000;
-        t.assert(price > bear_price);
+        t.assert(price.greaterThan(bear_price));
     }
 
     {
         let token = "ETH";
         let price = await get_price(token);
         let bear_price = 500;
-        t.assert(price > bear_price);
+        t.assert(price.greaterThan(bear_price));
     }
 
     {
         let token = "XRP";
         let price = await get_price(token);
         let bear_price = 0.1;
-        t.assert(price > bear_price);
+        t.assert(price.greaterThan(bear_price));
     }
 })
+
+test('Get Portfolio', async (t) => {
+
+    let output = [
+        {
+            token: "BTC",
+            balance: new Decimal(0.298660).mul(10000),
+        },
+        {
+            token: "ETH",
+            balance: new Decimal(0.271230).mul(500),
+        },
+        {
+            token: "XRP",
+            balance: new Decimal(1.509647).mul(0.1),
+        }
+    ]
+
+    let input = "src/tests/transaction_test.csv"
+
+    let portfolio = await get_latest_portfolio(input);
+
+    for (let i = 0; i < portfolio.length; i++) {
+        t.assert(portfolio[i].balance.greaterThan(output[i].balance));
+    }
+
+})
+
+test('Get Token Portfolio', async (t) => {
+
+    let output = [
+        {
+            token: "BTC",
+            balance: new Decimal(0.298660).mul(10000),
+        },
+        {
+            token: "ETH",
+            balance: new Decimal(0.271230).mul(500),
+        },
+        {
+            token: "XRP",
+            balance: new Decimal(1.509647).mul(0.1),
+        }
+    ]
+
+    let input = "src/tests/transaction_test.csv"
+
+    for (let i = 0; i < output.length; i++) {
+        let portfolio = await get_latest_portfolio_by_token(output[i].token, input);
+
+        t.assert(portfolio.balance.greaterThan(output[i].balance));
+    }
+
+})
+
